424ad00897f8fadc3e89a4c047c8978077035dd3
halloj3 <halloj3@frenchhorn.ee.washington.edu>
2016-02-01 16:51:18 -0800

    Checking in edits to dripExtract modulde

--- dripExtract.py
+++ dripExtract.py
@@ -71,6 +71,12 @@ def process_args(args):
         - Relevant directories will be first removed then created, boolean parameters
           will be adjusted to reflect their boolean command line strings
     """
+    args.filt_theo_peaks = True
+
+    # set absolute path of learned means and covariance
+    args.learned_means = os.path.abspath(args.learned_means)
+    args.learned_covars = os.path.abspath(args.learned_covars)
+
     # check input arguments, create necessary directories, 
     # set necessary global variables based on input arguments
     # constant string
@@ -79,7 +85,8 @@ def process_args(args):
         exit(-1)
     # check arguments
     assert(args.shards > 0)
-    if not args.output_file or not args.fasta:
+    # if not args.output_file or not args.fasta:
+    if not args.output_file:
         parser.print_help()
         exit(8)
 
@@ -132,14 +139,14 @@ def process_args(args):
         args.ident = ''
 
     # set true or false strings to booleans
-    args.load_peptide_database_file = df.check_arg_trueFalse(args.load_peptide_database_file)
     args.write_pin = df.check_arg_trueFalse(args.write_pin)
     args.append_to_pin = df.check_arg_trueFalse(args.append_to_pin)
-    args.decoys = df.check_arg_trueFalse(args.decoys)
     args.monoisotopic_precursor = df.check_arg_trueFalse(args.monoisotopic_precursor)
     args.high_res_ms2 = df.check_arg_trueFalse(args.high_res_ms2)
     args.randomize_ms2_spectra = df.check_arg_trueFalse(args.randomize_ms2_spectra)
 
+    # args.decoys = df.check_arg_trueFalse(args.decoys)
+    # args.load_peptide_database_file = df.check_arg_trueFalse(args.load_peptide_database_file)
     # check precursor mass type
     pmt = args.precursor_window_type.lower()
     if pmt == 'da':
@@ -469,7 +476,7 @@ def runDripExtract(args, stdo, stde):
         create_drip_master(args.high_res_ms2, args.master_file, 
                            args.max_obs_mass,
                            "DRIP_MZ",
-                           "inverted_collection/covar.txt",
+                           "drip_collection/covar.txt",
                            "DRIP_GAUSSIAN_COMPONENTS",
                            "DRIP_GAUSSIAN_MIXTURES",
                            "DRIP_MZ_GAUSSIANS")
@@ -483,7 +490,6 @@ def runDripExtract(args, stdo, stde):
         print "Could not create triangulate structure file %s, exitting" % args.structure_file
         exit(-1)
 
-    print args.covar_file
     try:
         write_covar_file(args.high_res_ms2, args.covar_file,
                          args.learned_covars)
@@ -561,9 +567,9 @@ if __name__ == '__main__':
     searchParamsGroup.add_argument('--charges', type = str, action = 'store', default = 'All', help = help_charges)
     help_high_res_ms2 = """<T|F> - boolean, whether the search is over high-res ms2 (high-high) spectra. When this parameter is true, DRIP used the real valued masses of candidate peptides as its Gaussian means. For low-res ms2 (low-low or high-low), the observed m/z measures are much less accurate so these Gaussian means are learned using training data (see dripTrain). Default=False."""
     searchParamsGroup.add_argument('--high-res-ms2', type = str, action = 'store', default = 'false', help = help_high_res_ms2)
-    help_decoys = '<T|F> - whether to create (shuffle target peptides) and search decoy peptides. Default = True'
-    searchParamsGroup.add_argument('--decoys', type = str, action = 'store', 
-                                   default = 'True', help = help_decoys)
+    # help_decoys = '<T|F> - whether to create (shuffle target peptides) and search decoy peptides. Default = True'
+    # searchParamsGroup.add_argument('--decoys', type = str, action = 'store', 
+    #                                default = 'True', help = help_decoys)
     help_num_threads = '<integer> - the number of threads to run on a multithreaded CPU. If supplied value is greater than number of supported threads, defaults to the maximum number of supported threads minus one. Multithreading is not suppored for cluster use as this is typically handled by the cluster job manager. Default=1.'
     searchParamsGroup.add_argument('--num-threads', type = int, action = 'store', 
                                    default = 1, help = help_num_threads)
@@ -586,13 +592,13 @@ if __name__ == '__main__':
     max_length_help = """<integer> - The maximum length of peptides to consider. Default = 50."""
     peptidePropertiesGroup.add_argument('--max-length', type = int, action = 'store',
                                         default = 50, help = max_length_help)
-    max_mass_help = """<foat> - The maximum mass (in Da) of peptides to consider. Default = 7200."""
+    max_mass_help = """<float> - The maximum mass (in Da) of peptides to consider. Default = 7200."""
     peptidePropertiesGroup.add_argument('--max-mass', type = float, action = 'store',
                                         default = 7200.0, help = max_mass_help)
     min_length_help = """<integer> - The minimum length of peptides to consider. Default = 6."""
     peptidePropertiesGroup.add_argument('--min-length', type = int, action = 'store',
                                         default = 6, help = min_length_help)
-    min_mass_help = """<foat> - The minimum mass (in Da) of peptides to consider. Default = 7200."""
+    min_mass_help = """<float> - The minimum mass (in Da) of peptides to consider. Default = 7200."""
     peptidePropertiesGroup.add_argument('--min-mass', type = float, action = 'store',
                                         default = 200.0, help = min_mass_help)
     monoisotopic_precursor_help = """<T|F> - When computing the mass of a peptide, use monoisotopic masses rather than average masses. Default = true."""
@@ -621,17 +627,17 @@ if __name__ == '__main__':
     help_seed = '<seed> - When given a unsigned integer value seeds the random number generator with that value. When given the string "time" seeds the random number generator with the system time. Default = 1.'
     decoyDBGroup.add_argument('--seed', type = str, action = 'store', 
                               default = '1', help = help_seed)
-    ############ enzymatic digestion
-    enzymeDigestGroup = parser.add_argument_group('enzymeDigestGroup', 'Options for the digesting enzyme.')
-    enzyme_help = """"<no-enzyme|trypsin|trypsin/p|chymotrypsin|elastase|clostripain|cyanogen-bromide|iodosobenzoate|proline-endopeptidase|staph-protease|asp-n|lys-c|lys-n|arg-c|glu-c|pepsin-a|elastase-trypsin-chymotrypsin|custom-enzyme> - Specify the enzyme used to digest the proteins in silico. Available enzymes (with the corresponding digestion rules indicated in parentheses) include no-enzyme ([X]|[X]), trypsin ([RK]|{P}), trypsin/p ([RK]|[]), chymotrypsin ([FWYL]|{P}), elastase ([ALIV]|{P}), clostripain ([R]|[]), cyanogen-bromide ([M]|[]), iodosobenzoate ([W]|[]), proline-endopeptidase ([P]|[]), staph-protease ([E]|[]), asp-n ([]|[D]), lys-c ([K]|{P}), lys-n ([]|[K]), arg-c ([R]|{P}), glu-c ([DE]|{P}), pepsin-a ([FL]|{P}), elastase-trypsin-chymotrypsin ([ALIVKRWFY]|{P}). Specifying --enzyme no-enzyme yields a non-enzymatic digest. Warning: the resulting peptide database may be quite large. Default = trypsin."""
-    enzymeDigestGroup.add_argument('--enzyme', type = str, action = 'store',
-                                   default = 'trypsin', help = enzyme_help)
-    custom_enzyme_help = """<string> - Specify rules for in silico digestion of protein sequences. Overrides the enzyme option. Two lists of residues are given enclosed in square brackets or curly braces and separated by a |. The first list contains residues required/prohibited before the cleavage site and the second list is residues after the cleavage site. If the residues are required for digestion, they are in square brackets, '[' and ']'. If the residues prevent digestion, then they are enclosed in curly braces, '{' and '}'. Use X to indicate all residues. For example, trypsin cuts after R or K but not before P which is represented as [RK]|{P}. AspN cuts after any residue but only before D which is represented as [X]|[D]. Default = <empty>."""
-    enzymeDigestGroup.add_argument('--custom-enzyme', type = str, action = 'store',
-                                   default = '', help = custom_enzyme_help)
-    missed_cleavages_help = """<integer> - Maximum number of missed cleavages per peptide to allow in enzymatic digestion. Default = 0."""
-    enzymeDigestGroup.add_argument('--missed-cleavages', type = int, action = 'store',
-                                   default = 0, help = custom_enzyme_help)
+    # ############ enzymatic digestion
+    # enzymeDigestGroup = parser.add_argument_group('enzymeDigestGroup', 'Options for the digesting enzyme.')
+    # enzyme_help = """"<no-enzyme|trypsin|trypsin/p|chymotrypsin|elastase|clostripain|cyanogen-bromide|iodosobenzoate|proline-endopeptidase|staph-protease|asp-n|lys-c|lys-n|arg-c|glu-c|pepsin-a|elastase-trypsin-chymotrypsin|custom-enzyme> - Specify the enzyme used to digest the proteins in silico. Available enzymes (with the corresponding digestion rules indicated in parentheses) include no-enzyme ([X]|[X]), trypsin ([RK]|{P}), trypsin/p ([RK]|[]), chymotrypsin ([FWYL]|{P}), elastase ([ALIV]|{P}), clostripain ([R]|[]), cyanogen-bromide ([M]|[]), iodosobenzoate ([W]|[]), proline-endopeptidase ([P]|[]), staph-protease ([E]|[]), asp-n ([]|[D]), lys-c ([K]|{P}), lys-n ([]|[K]), arg-c ([R]|{P}), glu-c ([DE]|{P}), pepsin-a ([FL]|{P}), elastase-trypsin-chymotrypsin ([ALIVKRWFY]|{P}). Specifying --enzyme no-enzyme yields a non-enzymatic digest. Warning: the resulting peptide database may be quite large. Default = trypsin."""
+    # enzymeDigestGroup.add_argument('--enzyme', type = str, action = 'store',
+    #                                default = 'trypsin', help = enzyme_help)
+    # custom_enzyme_help = """<string> - Specify rules for in silico digestion of protein sequences. Overrides the enzyme option. Two lists of residues are given enclosed in square brackets or curly braces and separated by a |. The first list contains residues required/prohibited before the cleavage site and the second list is residues after the cleavage site. If the residues are required for digestion, they are in square brackets, '[' and ']'. If the residues prevent digestion, then they are enclosed in curly braces, '{' and '}'. Use X to indicate all residues. For example, trypsin cuts after R or K but not before P which is represented as [RK]|{P}. AspN cuts after any residue but only before D which is represented as [X]|[D]. Default = <empty>."""
+    # enzymeDigestGroup.add_argument('--custom-enzyme', type = str, action = 'store',
+    #                                default = '', help = custom_enzyme_help)
+    # missed_cleavages_help = """<integer> - Maximum number of missed cleavages per peptide to allow in enzymatic digestion. Default = 0."""
+    # enzymeDigestGroup.add_argument('--missed-cleavages', type = int, action = 'store',
+    #                                default = 0, help = custom_enzyme_help)
     ############ shard spectra options
     parser.add_argument('--min_spectrum_length', type = int, action = 'store',
                         dest = 'min_spectrum_length', default = 1)
@@ -657,9 +663,9 @@ if __name__ == '__main__':
     parser.add_argument('--normalize', dest = "normalize", type = str,
                         help = "Name of the spectrum preprocessing pipeline.", 
                         default = 'top300TightSequest')
-    help_filt_theo_peaks = "Filter theoretical peaks outside of observed spectra min and max"
-    parser.add_argument('--filt_theo_peaks', action = 'store_true', dest = 'filt_theo_peaks', 
-                        default = False, help = help_filt_theo_peaks)
+    # help_filt_theo_peaks = "Filter theoretical peaks outside of observed spectra min and max"
+    # parser.add_argument('--filt_theo_peaks', action = 'store_true', dest = 'filt_theo_peaks', 
+    #                     default = False, help = help_filt_theo_peaks)
     parser.add_argument('--per_spectrum_mz_bound', action = 'store_true', 
                         default = False, help = "Calculate observed m/z bound per spectrum")
     parser.add_argument('--mz_lb', type = float, action = 'store',
@@ -718,12 +724,12 @@ if __name__ == '__main__':
     help_output_file = '<string> - Output file for resulting peptides.'
     oFileGroup.add_argument('--output_file', type = str, action = 'store', 
                             default = 'peptides.txt', help = help_output_file)
-    help_peptide_database_file = """<string> - Output file for resulting peptides. Assumed to be binary format (i.e., python pickle file) for quick future access. See target-output-file/decoy-output-file options for ASCII file equivalents. Default = None."""
-    oFileGroup.add_argument('--peptide-database-file', type = str, action = 'store', 
-                            default = '', help = help_peptide_database_file)
-    help_load_peptide_database_file = """<T|F> - Load previously digested peptide database file. If the supplied --peptide-database-file does not exist, the protein database will be digested. If decoys is true and no decoys are contained in the pickle, decoys will be created on the fly. Default = F."""
-    oFileGroup.add_argument('--load-peptide-database-file', type = str, action = 'store', 
-                            default = 'False', help = help_load_peptide_database_file)
+    # help_peptide_database_file = """<string> - Output file for resulting peptides. Assumed to be binary format (i.e., python pickle file) for quick future access. See target-output-file/decoy-output-file options for ASCII file equivalents. Default = None."""
+    # oFileGroup.add_argument('--peptide-database-file', type = str, action = 'store', 
+    #                         default = '', help = help_peptide_database_file)
+    # help_load_peptide_database_file = """<T|F> - Load previously digested peptide database file. If the supplied --peptide-database-file does not exist, the protein database will be digested. If decoys is true and no decoys are contained in the pickle, decoys will be created on the fly. Default = F."""
+    # oFileGroup.add_argument('--load-peptide-database-file', type = str, action = 'store', 
+    #                         default = 'False', help = help_load_peptide_database_file)
     oFileGroup.add_argument('--logDir', type = str, 
                       help = 'directory with output log files')
     oFileGroup.add_argument('--output', type = str,
@@ -734,10 +740,6 @@ if __name__ == '__main__':
     process_args(args)
 
     stde = open('gmtk_err', "w")
-
-    # set absolute path of learned means and covariance
-    args.learned_means = os.path.abspath(args.learned_means)
-    args.learned_covars = os.path.abspath(args.learned_covars)
 
     if debug:
         stdo = sys.stdout
@@ -797,7 +799,7 @@ if __name__ == '__main__':
             create_drip_master(args.high_res_ms2, args.master_file, 
                                args.max_obs_mass,
                                "DRIP_MZ",
-                               "inverted_collection/covar.txt",
+                               "drip_collection/covar.txt",
                                "DRIP_GAUSSIAN_COMPONENTS",
                                "DRIP_GAUSSIAN_MIXTURES",
                                "DRIP_MZ_GAUSSIANS")
@@ -868,35 +870,42 @@ if __name__ == '__main__':
     ####################################################################################
 
     if args.write_pin:
-        if not args.append_to_pin: # load database features to write drip output
-            # database information required
-            # formulate digestion regular expression
-            digest_re = df.create_digest_re(args.enzyme, args.custom_enzyme)
-            print "digest regular expression: %s" % digest_re
-            r = re.compile(digest_re)
+        # if not args.append_to_pin: # load database features to write drip output
+        #     # database information required
+        #     # formulate digestion regular expression
+        #     digest_re = df.create_digest_re(args.enzyme, args.custom_enzyme)
+        #     print "digest regular expression: %s" % digest_re
+        #     r = re.compile(digest_re)
 
-            # parse modifications
-            mods = df.parse_mods(args.mods_spec, True)
-            print "mods:"
-            print mods
-            nterm_mods = df.parse_mods(args.nterm_peptide_mods_spec, False)
-            print "n-term mods:"
-            print nterm_mods
-            cterm_mods = df.parse_mods(args.cterm_peptide_mods_spec, False)
-            print "c-term mods:"
-            print cterm_mods
+        #     # parse modifications
+        #     mods = df.parse_mods(args.mods_spec, True)
+        #     print "mods:"
+        #     print mods
+        #     nterm_mods = df.parse_mods(args.nterm_peptide_mods_spec, False)
+        #     print "n-term mods:"
+        #     print nterm_mods
+        #     cterm_mods = df.parse_mods(args.cterm_peptide_mods_spec, False)
+        #     print "c-term mods:"
+        #     print cterm_mods
 
-            cleavage_req = df.enzyme_enzSet(args.enzyme, args.custom_enzyme)
-            target_db, decoy_db = load_target_decoy_db_no_reshuffle(args, r, mods, nterm_mods, cterm_mods)
-            psm.write_percolator_pin(targets, decoys, args.output, args.mean_file, spec_dict, 
-                                     cleavage_req, target_db, decoy_db)
-        else: # don't worry about database features, just append DRIP features to PIN file's features
-            targets0,decoys0 = load_pin_return_dict(args.psm_file)
-            # merge the loaded PSM features with DRIP
-            psm.append_to_percolator_pin(targets, decoys, 
-                                         targets0, decoys0,
-                                         args.output, 
-                                         args.mean_file, spec_dict)
+        #     cleavage_req = df.enzyme_enzSet(args.enzyme, args.custom_enzyme)
+        #     target_db, decoy_db = load_target_decoy_db_no_reshuffle(args, r, mods, nterm_mods, cterm_mods)
+        #     psm.write_percolator_pin(targets, decoys, args.output, args.mean_file, spec_dict, 
+        #                              cleavage_req, target_db, decoy_db)
+        # else: # don't worry about database features, just append DRIP features to PIN file's features
+        #     targets0,decoys0 = load_pin_return_dict(args.psm_file)
+        #     # merge the loaded PSM features with DRIP
+        #     psm.append_to_percolator_pin(targets, decoys, 
+        #                                  targets0, decoys0,
+        #                                  args.output, 
+        #                                  args.mean_file, spec_dict)
+        targets0,decoys0 = load_pin_return_dict(args.psm_file)
+        # merge the loaded PSM features with DRIP
+        psm.append_to_percolator_pin(targets, decoys, 
+                                     targets0, decoys0,
+                                     args.output, 
+                                     args.mean_file, spec_dict)
+
     else:
         psm.write_output(targets, decoys, args.output, args.mean_file, spec_dict)
 
